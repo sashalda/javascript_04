e8c03044ab1b69e8271de972a3694be7784d66be
sashalda <sdod.26@gmail.com>
2021-10-21 12:47:09 -0300

    arreglos funciones

--- main.js
+++ main.js
@@ -1,24 +1,45 @@
-let libro = prompt('Ingrese el nombre de su libro').toLowerCase();
-let paginas = parseInt(prompt('Ingrese la cantidad de páginas'));
-let dificultad = 0;
-let hora = 50;
-
-switch (libro) {
-
-    case "el principito":
-        dificultad= 1.0;
-        break;
-    case "el psicoanalista":
-        dificultad= 1.8;
-        break;
-    case "la biblia":
-        dificultad = 2.2;
-        break;
-    case "los tres chanchitos":
-        dificultad = 1.2;
-        break;    
-    default:
-        alert("El libro solicitado no se encuentra en nuestra base de datos")
+function pedirNombre () {
+   return prompt('Ingrese el nombre de su libro').toLowerCase();   
 }
 
-alert("El tiempo de lectura de " + libro + " es de " +(paginas/hora) * dificultad .toFixed(2) + " horas" );
+let libro = pedirNombre();
+
+function pedirPaginas() {
+    return parseInt(prompt('Ingrese la cantidad de páginas'));
+}
+
+let paginas = pedirPaginas();
+
+const hora = 50;
+
+function obtenerDificultad (libro) {
+    
+    let dificultad = 0;
+    
+    switch (libro) {
+
+        case "el principito":
+            dificultad = 1.0;
+            break;
+        case "el psicoanalista":
+            dificultad = 1.8;
+            break;
+        case "la biblia":
+            dificultad = 2.2;
+            break;
+        case "los tres chanchitos":
+            dificultad = 1.2;
+            break;    
+        default:
+            alert("El libro solicitado no se encuentra en nuestra base de datos");
+    }
+    return dificultad;
+}
+
+let dificultad = obtenerDificultad(libro);
+
+function calcularTiempo(paginas, hora, dificultad) {
+    return ((paginas/hora)*dificultad).toFixed(2);
+}
+
+alert("El tiempo de lectura de " + libro + " es de " + calcularTiempo(paginas, hora, dificultad) + " horas" );
